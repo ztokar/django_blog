84558c431f20d99d4dc49806dbd8d52b9fd55354
Zachary Tokar <zack.tokar@gmail.com>
2023-07-19 13:19:36 -0400

    Finished entry and topic page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -44,8 +44,8 @@ def new_entry(request, topic_id):
     if request.method != 'POST':
         form = EntryForm()
     else:
-        form=EntryForm(data=request.post)
-        if form.us_valid():
+        form=EntryForm(data=request.POST)
+        if form.is_valid():
             new_entry=form.save(commit=False)
             new_entry.topic=topic
             new_entry.save()
