de4e45013200736794f58b94a809de824704b115
Zachary Tokar <zack.tokar@gmail.com>
2023-07-18 23:10:49 -0400

    Finished entry and topic htmls

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import urls
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -17,6 +18,37 @@ def topics(request):
 def topic(request,topic_id):
     """Show a single topic and all its entries"""
     topic=Topic.objects.get(id=topic_id)
-    entries=topic.entry_set.order_by('-date_added')
+    entries=topic.entry_set.order_by('date_added')
     context={'topic':topic,'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Let a user add a new topic"""
+    if request.method != 'POST':
+        #no data submitted; create a blank form
+        form= TopicForm()
+    else:
+        #POST data submitted, process data
+        form=TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    #Display a blank or invalid form
+    context={'form':form}
+    return render(request,'learning_logs/new_topic.html',context)
+
+def new_entry(request, topic_id):
+    """"Let a user add a new entry"""
+    topic=Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form=EntryForm(data=request.post)
+        if form.us_valid():
+            new_entry=form.save(commit=False)
+            new_entry.topic=topic
+            new_entry.save()
+            return redirect('learning_logs:topic',topic_id=topic_id)
+    context={'topic':topic,'form':form}
+    return render(request, 'learning_logs/new_entry.html',context)
