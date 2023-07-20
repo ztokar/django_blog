0e6913a80af5b8af9939b4420d75d1f2101c5518
Zachary Tokar <zack.tokar@gmail.com>
2023-07-19 23:38:10 -0400

    Edit entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from . import urls
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -52,3 +52,19 @@ def new_entry(request, topic_id):
             return redirect('learning_logs:topic',topic_id=topic_id)
     context={'topic':topic,'form':form}
     return render(request, 'learning_logs/new_entry.html',context)
+
+def edit_entry(request, entry_id):
+    """Leta user edit an old entry"""
+    entry=Entry.objects.get(id=entry_id)
+    topic=entry.topic
+
+    if request.method != 'POST':
+        form=EntryForm(instance=entry)
+    else:
+        form=EntryForm(instance=entry,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic',topic_id=topic.id)
+        
+    context= {'entry': entry,'topic':topic,'form':form}
+    return render(request,'learning_logs/edit_entry.html',context)
